85684c65d51e48225a78b9d7f4a6bdfbbc5a6593
Viktotovich <148013724+Viktotovich@users.noreply.github.com>
2025-02-28 02:33:37 +0400

    Created the model for messages, and altered the existing users_and_passwords model.
    Additionally, increased the iteration count for hashing as per reccomendations.

--- db/populatedb.js
+++ db/populatedb.js
@@ -28,13 +28,30 @@ const query = `
 
   CREATE TABLE IF NOT EXISTS "users_and_passwords" (
   "id" INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  "fullname" VARCHAR(255) NOT NULL,
+  "membership_status" VARCHAR NOT NULL,
   "username" VARCHAR(255) NOT NULL UNIQUE,
   "hash" VARCHAR NOT NULL,
   "salt" VARCHAR
   );
 
   CREATE INDEX IF NOT EXISTS "idx_username" ON "users_and_passwords" ("username");
+
+  CREATE TABLE IF NOT EXISTS "messages" (
+  "message_id" INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  "message_content" VARCHAR NOT NULL,
+  "created_at" TIMESTAMP DEFAULT NOW(),
+  "user_id" INTEGER,
+  CONSTRAINT fk_user
+    FOREIGN KEY (user_id)
+      REFERENCES users_and_passwords(id)
+        ON DELETE CASCADE
+  );
 `;
+
+/*TODO:
+  Learn how to make ENUM types so that fields like membership_status can only be a set value belonging to an array 
+*/
 
 async function start() {
   console.log("seeding");
