436aab8e4aef753375a5190f812c465a7c100661
Viktotovich <148013724+Viktotovich@users.noreply.github.com>
2025-03-04 21:17:13 +0400

    Created port-display partial to be used for in 2 pages (Index & All Posts), a membership_status checker to outsource logic, and made minor adjustments to login-success

--- controllers/postController.js
+++ controllers/postController.js
@@ -2,8 +2,18 @@ const db = require("../db/queries");
 const links = require("../links");
 const { validationResult } = require("express-validator");
 
-module.exports.getPostsMain = (req, res) => {
+module.exports.getPostsMain = async (req, res) => {
   const title = "All messages";
-  const messages = db.getAllMessages();
+  const posts = await db.getAllMessages();
+  const approved = checkApproval(req.user.membership_status);
+
   //
 };
+
+function checkApproval(membership_status) {
+  if (membership_status === "Member") {
+    return true;
+  } else {
+    return false;
+  }
+}
